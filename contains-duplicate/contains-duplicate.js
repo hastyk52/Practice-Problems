cf5b949f8e6bacd69157eb701a4cb45b7be14c34
Keanu Hasty <65418122+hastyk52@users.noreply.github.com>
2021-09-29 15:32:47 -0600

    Time: 88 ms (62.56%), Space: 46.8 MB (27.94%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    var count = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (count[nums[i]]) {
+            return true;
+        } else {
+            count[nums[i]] = true;
+        }        
+    }
+    return false;
+};
