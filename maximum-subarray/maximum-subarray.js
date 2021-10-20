50c16a2fa7f7c95b46877f8facd4051ec508ae28
Keanu Hasty <65418122+hastyk52@users.noreply.github.com>
2021-10-20 08:28:59 -0600

    Time: 143 ms (18.53%), Space: 48.6 MB (36.97%) - LeetHub

--- maximum-subarray/maximum-subarray.js
+++ maximum-subarray/maximum-subarray.js
@@ -2,35 +2,9 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-// Given an arrayof integers, find the contiguous sub array with the largest sum and returns that sum
-
-// I: Array of integers
-// O: Largest sum resulting from contiguous subarray
-// C: subarray must contain at least 1 number
-//    1 <= nums.length <= 10^5
-//    -10^4 <= nums[i] <= 10^4
-// E: Empty array
-//    array has length of 1
-//    
-
-// handle empty array
-
-// initialize sum and max sum as first position
-// iterate array starting at i = 1
-  // if sum is negative
-    // sum = nums[i]
-  // else
-    // sum += nums[i]
-  // maxSum = max(sum, maxSum)
-
-// return maxSum
-
-var maxSubArray = function(nums) {
-    if (nums.length === 0) return;
-    
+var maxSubArray = function(nums) {    
     let sum = nums[0];
-    let max = nums[0];
+    let max = sum;
     
     for (let i = 1; i < nums.length; i++) {
         if (sum < 0) {
@@ -38,7 +12,9 @@ var maxSubArray = function(nums) {
         } else {
             sum += nums[i];
         }
-        max = Math.max(sum, max);
+        if (sum > max) {
+            max = sum;
+        }
     }
     
     return max;
